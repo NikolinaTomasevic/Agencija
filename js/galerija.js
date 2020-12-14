eee5405ce8b833f278d55dbb2f8b2151ec9b7710
NikolinaTomasevic <nikolina.tomasevic2000@gmail.com>
2020-12-14 17:49:49 +0100

    initial-git

---
+++ js/galerija.js
@@ -0,0 +1,40 @@
+// Otvori Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // ZAtvori Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Listanje slika
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Otvaranje konkretne slike
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
